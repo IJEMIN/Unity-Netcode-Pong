462cc98d8b9c16112dfdc76edd09bc98f9aa58c9
i_jemin <jemin.lee@unity3d.com>
2023-03-20 15:55:32 +0900

    Player 스크립트를 NetCode로 변경

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,40 @@
-﻿// using Photon.Pun;
-// using UnityEngine;
-//
-// public class Player : MonoBehaviourPun
-// {
-//     private Rigidbody2D playerRigidbody;
-//     private SpriteRenderer spriteRenderer;
-//     
-//     public float speed = 3f;
-//
-//     private void Start()
-//     {
-//         playerRigidbody = GetComponent<Rigidbody2D>();
-//         spriteRenderer = GetComponent<SpriteRenderer>();
-//         
-//         if (photonView.IsMine)
-//         {
-//             spriteRenderer.color = Color.blue;
-//         }
-//         else
-//         {
-//             spriteRenderer.color = Color.red;
-//         }
-//     }
-//
-//     private void Update()
-//     {
-//         if (!photonView.IsMine)
-//         {
-//             return;
-//         }
-//
-//         var input = InputButton.VerticalInput;
-//         
-//         var distance = input * speed * Time.deltaTime;
-//         var targetPosition = transform.position + Vector3.up * distance;
-//         
-//         playerRigidbody.MovePosition(targetPosition);
-//     }
-// }
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    private SpriteRenderer spriteRenderer;
+    
+    public float speed = 3f;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        if (IsOwner)
+        {
+            spriteRenderer.color = Color.blue;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void Update()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        var input = Input.GetAxis("Vertical");
+        
+        var distance = input * speed * Time.deltaTime;
+        var targetPosition = transform.position + Vector3.up * distance;
+        
+        playerRigidbody.MovePosition(targetPosition);
+    }
+}
